9a8d2401d0418ef82d101a3bcb1d899543b69d0d
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-26 05:19:31 +0530

    time o(n) || solved by setting a range for leftTree and rightTree
    Constraints :
    Time Limit: 1 second
    Sample Input 1 :
    3 1 5 -1 2 -1 -1 -1 -1
    Sample Output 1 :
    true
    Sample Input 2 :
    5 2 10 0 1 -1 15 -1 -1 -1 -1 -1 -1
    Sample Output 2 :
    false

---
+++ Day 19/isBSTM3.java
@@ -0,0 +1,179 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class QueueEmptyException extends Exception {
+
+}
+
+ class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+public class Runner {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+		QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>(); 
+		int start = 0;
+
+		String[] nodeDatas = br.readLine().trim().split(" ");
+
+		if (nodeDatas.length == 1) {
+			return null;
+		}
+		
+		int rootData = Integer.parseInt(nodeDatas[start]);
+		start += 1;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+
+			int leftChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+
+			int rightChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+
+		return root;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+
+        boolean ans = Solution.isBST(root);
+        
+        System.out.println(ans);
+
+	}
+}
+
+
+
+// method 3
+// time o(n);
+
+
+// in this approach we are not putting any restrictions on root 
+// root can take any value 
+// idea here is the leftTree and the rightTree can have values within a range only;
+// left -> (min, root.data-1);
+// right -> (root.data, max);
+
+public class Solution {
+
+	public static boolean isBST(BinaryTreeNode<Integer> root) {
+		return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+    
+    private static boolean helper(BinaryTreeNode<Integer> root, int min, int max){
+        if(root == null)
+            return true;
+        
+        if(root.data < min || root.data > max)
+            return false;
+        
+        boolean isLeftBST = helper(root.left, min, root.data-1);
+        boolean isRightBST = helper(root.right, root.data, max);
+        
+        return isLeftBST && isRightBST;
+    }
+
+}
+
