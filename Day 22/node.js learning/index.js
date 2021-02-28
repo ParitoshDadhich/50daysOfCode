12d9f7de969dc2b94746030b342f0f6fb7249b3e
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-28 23:28:47 +0530

    Add files via upload

---
+++ Day 22/node.js learning/index.js
@@ -0,0 +1,9 @@
+var superheroes = require("superheroes");
+var myHero = superheroes.random();
+
+console.log(myHero);
+
+const supervillains = require('supervillains');
+
+var myVillain = supervillains.random();
+console.log(myVillain);
