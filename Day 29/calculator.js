a73fcbc4a45d954124325e986a27afa09c845eca
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-03-09 02:14:12 +0530

    calculator set up

---
+++ Day 29/calculator.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.get("/", function(req, res){
+    res.send("Hello world");
+});
+
+app.listen(3000, function(){
+    console.log("app is listening to port 3000");
+});
